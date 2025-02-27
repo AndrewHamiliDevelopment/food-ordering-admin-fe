b2cde798b6e8611569dd1952dd355746a6c8fc99
andrewhamili <andrewhamilidevelopment@gmail.com>
2025-02-27 20:22:23 +0800

    Setup initial

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,10 @@ function App() {
         store.paginatedCategories = responses[1].data as Paginated<Category>;
         store.paginatedPaymentMethods = responses[2]
           .data as Paginated<PaymentMethod>;
-        //setIsReady(true);
+        setIsReady(true);
       } catch (error) {
         console.error("error", error);
+        throw new Error("Error fetching initial data");
       }
     };
 
@@ -40,33 +41,37 @@ function App() {
   }, []);
 
   React.useEffect(() => {
-    console.log("🚀 ~ React.useEffect ~ snap:", snap)
+    console.log("🚀 ~ React.useEffect ~ snap:", snap);
   }, [snap]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+  if (!isReady) return <p>Loading...</p>;
+
+  if (isReady) {
+    return (
+      <>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default App;
